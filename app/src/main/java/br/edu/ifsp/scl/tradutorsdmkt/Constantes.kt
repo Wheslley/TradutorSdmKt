f6902ab3782ff2cc53cf9a4ae287bbc8368966bf
Wheslley <wheslleyny@gmail.com>
2019-03-28 21:50:14 -0300

    Tradutor

---
+++ app/src/main/java/br/edu/ifsp/scl/tradutorsdmkt/Constantes.kt
@@ -0,0 +1,11 @@
+package br.edu.ifsp.scl.tradutorsdmkt
+
+object Constantes {
+    // Constantes usadas para acesso ao WS de tradução
+    val URL_BASE = "https://od-api.oxforddictionaries.com/api/v1/"
+    val END_POINT = "entries"
+    val APP_ID_FIELD = "app_id"
+    val APP_ID_VALUE = "7e7e58eb" // Preencher com seu app_id
+    val APP_KEY_FIELD = "app_key"
+    val APP_KEY_VALUE = "f95dc06bb1389ec0d66e3b5d0b175514" // Preeencher com seu app_key
+}
